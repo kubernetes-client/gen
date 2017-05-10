5ee5ed6155d94b6e253039b5f05b5b11949da317
mbohlool <mehdy@google.com>
2017-05-10 10:42:41 -0700

    Add ruby client generator

---
+++ openapi/ruby.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2015 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ARGC=$#
+
+if [ $# -ne 2 ]; then
+    echo "Usage:\n\n\truby.sh OUTPUT_DIR SETTING_FILE_PATH"
+    echo "\nSetting file should define KUBERNETES_BRANCH, CLIENT_VERSION, and PACKAGE_NAME\n"
+    exit 1
+fi
+
+
+OUTPUT_DIR=$1
+SETTING_FILE=$2
+mkdir -p "${OUTPUT_DIR}"
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
+pushd "${SCRIPT_ROOT}" > /dev/null
+SCRIPT_ROOT=`pwd`
+popd > /dev/null
+
+pushd "${OUTPUT_DIR}" > /dev/null
+OUTPUT_DIR=`pwd`
+popd > /dev/null
+
+source "${SCRIPT_ROOT}/client-generator.sh"
+source "${SETTING_FILE}"
+
+CLIENT_LANGUAGE=ruby; CLEANUP_DIRS=(docs lib spec); kubeclient::generator::generate_client "${OUTPUT_DIR}"
