3288249a23545264361ae80071556470e57a78e1
marie-yau <yau.marie@gmail.com>
2018-10-26 19:11:46 +0000

    fix: gen/openapi/python.sh - 'no such file or directory' exec error (#83)

--- openapi/python.sh
+++ openapi/python.sh
@@ -54,10 +54,10 @@ kubeclient::generator::generate_client "${OUTPUT_DIR}"
 echo "--- Patching generated code..."
 
 # workaround https://github.com/swagger-api/swagger-codegen/pull/8401
-find "${OUTPUT_DIR}/client/" -type f -name \*.py -exec sed -i 's/async=/async_req=/g' {} +
-find "${OUTPUT_DIR}/client/" -type f -name \*.py -exec sed -i 's/async bool/async_req bool/g' {} +
-find "${OUTPUT_DIR}/client/" -type f -name \*.py -exec sed -i "s/'async'/'async_req'/g" {} +
-sed -i "s/if not async/if not async_req/g" "${OUTPUT_DIR}/client/api_client.py"
+find "${OUTPUT_DIR}/" -type f -name \*.py -exec sed -i 's/async=/async_req=/g' {} +
+find "${OUTPUT_DIR}/" -type f -name \*.py -exec sed -i 's/async bool/async_req bool/g' {} +
+find "${OUTPUT_DIR}/" -type f -name \*.py -exec sed -i "s/'async'/'async_req'/g" {} +
+find "${OUTPUT_DIR}/" -name api_client.py -exec sed -i "s/if not async/if not async_req/g" {} +
 #
 
 find "${OUTPUT_DIR}/test" -type f -name \*.py -exec sed -i 's/\bclient/kubernetes.client/g' {} +
