9f439b68daead5af3218234931e68d3ce5acb25b
Tomasz Prus <tomasz.prus@gmail.com>
2020-02-11 21:34:10 +0100

    feat: switch python-asyncio to use openapi-generator v4.2.3 (#146)

--- openapi/python-asyncio.sh
+++ openapi/python-asyncio.sh
@@ -45,6 +45,7 @@ source "${SETTING_FILE}"
 
 # use openapi-generator to generate library
 source "${SCRIPT_ROOT}/openapi-generator/client-generator.sh"
+OPENAPI_GENERATOR_COMMIT="${OPENAPI_GENERATOR_COMMIT:-v4.2.3}"
 
 CLIENT_LANGUAGE=python-asyncio
 CLEANUP_DIRS=(client/apis client/models docs test)
